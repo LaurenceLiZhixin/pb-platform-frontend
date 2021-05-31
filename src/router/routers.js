b7f251db83e5db90c0d7d6ce37eabff929482afe
LaurenceLiZhixin <382673304@qq.com>
2021-06-01 00:01:57 +0800

    fix

--- src/router/routers.js
+++ src/router/routers.js
@@ -18,15 +18,15 @@ import parentView from '@/components/parent-view'
  */
 
 export default [
-  // {
-  //   path: '/login',
-  //   name: 'login',
-  //   meta: {
-  //     title: 'Login - 登录',
-  //     hideInMenu: true
-  //   },
-  //   component: () => import('@/view/login/login.vue')
-  // },
+   {
+     path: '/login',
+     name: 'login',
+     meta: {
+       title: 'Login - 登录',
+       hideInMenu: true
+     },
+     component: () => import('@/view/login/login.vue')
+   },
   {
     path: '/',
     name: '_home',
